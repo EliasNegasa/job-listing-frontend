7a23706cea3dfd3f6f9a0d04f26c835e7dca09bb
= <=>
2024-01-05 13:05:32 +0300

    layout and routes updates

--- src/router/index.js
+++ src/router/index.js
@@ -4,36 +4,36 @@ import Jobs from '../views/JobsView.vue'
 import Login from '../views/LoginView.vue'
 import NotFound from '../views/NotFoundView.vue'
 import { useAuthStore } from '../stores/auth'
-// import { storeToRefs } from 'pinia'
 
-// const authStore = useAuthStore()
-
-// const { isAuthenticated } = storeToRefs(authStore)
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/jobs',
+    name: 'Jobs',
+    component: Jobs,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/:notFound',
+    component: NotFound
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/jobs',
-      name: 'Jobs',
-      component: Jobs,
-      meta: { requiresAuth: true }
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    },
-    {
-      path: '/:notFound',
-      component: NotFound
-    }
-  ]
+  routes,
+  scrollBehavior() {
+    return { top: 0, left: 0, behavior: 'smooth' }
+  }
 })
 
 router.beforeEach((to, from, next) => {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,16 @@
 <script setup>
-import BaseHeader from '../components/BaseHeader.vue';
-
+import JobSearchForm from '@/components/JobSearchForm.vue'
 </script>
 
 <template>
   <main>
-    <BaseHeader />
+    <section class="flex items-center p-1 mt-10 dark:bg-gray-700">
+      <div class="container flex flex-col items-center">
+        <div class="flex flex-col max-w-md text-center">
+          <p class="text-2xl md:text-3xl dark:text-gray-400">Home Page</p>
+        </div>
+      </div>
+    </section>
+    <job-search-form />
   </main>
 </template>
