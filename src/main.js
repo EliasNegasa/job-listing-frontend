c13c20cb3eef1c006b6748e548f9034007bf4e6d
= <=>
2024-01-05 12:55:08 +0300

    font awesome added

--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faChevronRight, faChevronLeft, faFilter } from '@fortawesome/free-solid-svg-icons'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+library.add(faChevronRight, faChevronLeft, faFilter)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(createPinia())
 app.use(router)
